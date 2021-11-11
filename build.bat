2b3e8b476b9baaf27735e39f38eae4b5e7b121e3
NekokeCore <core2002@163.com>
2021-11-12 00:40:46 +0800

    在pc页面增加了右键单击功能

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cmd /c clean
-xcopy src\resources bin\
+xcopy src\resources bin\resources\
 
 go build -o bin\screenshot_debug.exe go_screenshot/src/main
 go build -o bin\screenshot_rel.exe -ldflags="-H windowsgui" go_screenshot/src/main
